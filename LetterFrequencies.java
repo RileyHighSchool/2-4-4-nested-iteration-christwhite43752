0f76c9606fde12759b557d5de760f3e966ce2486
ChristWhite43752 <christopher86152@students.sbcsc.k12.in.us>
2023-01-13 09:03:52 -0500

    frequency checker

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -1,15 +1,38 @@
 /*
  * Activity 2.4.4
  */
+import java.util.Scanner;
+
 public class LetterFrequencies
 {
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a string to see how many of each letter it has. ");
+
+
 		String letters = "abcdefghijklmnopqrstuvwxyz"; 
-		String phrase = "This is a phrase!";
+		String phrase = sc.nextLine();
+
+		phrase = phrase.toLowerCase();
 
 		System.out.println("The following shows the letter frequencies for the phrase");
 
     /* your code here */
+	for (int i=0; i<26; i++){
+		String letter = letters.substring(i,i+1);
+
+		int count = 0;
+		for (int inner=0; inner < phrase.length(); inner++ ){
+
+			String phraseLet = phrase.substring(inner, inner +1);
+
+			if (letter.equals(phraseLet)){
+				count++;
+			}
+		}
+		System.out.println(letter + " appears "+ count + " time(s).");
+		}
 	}
 }
